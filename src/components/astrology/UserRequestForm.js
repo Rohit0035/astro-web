aa9285c92b547b2b663103bfb2e5782fd4e0e4ba
Rohit0035 <rohitdevelopersveltosest@gmail.com>
2022-11-03 19:04:39 +0530

    userform

--- src/components/astrology/UserRequestForm.js
+++ src/components/astrology/UserRequestForm.js
@@ -7,8 +7,87 @@ import axiosConfig from "../../axiosConfig";
 import swal from "sweetalert";
 
 class UserRequestForm extends React.Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      userid: "",
+      fullname: "",
+      astroid: "",
+      gender: "",
+      tym_of_birth: "",
+      birth_city: "",
+      birth_state: "",
+      birth_country: "",
+      marital_status: "",
+      occupation: "",
+      topic_of_cnsrn: "",
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    let { id } = this.props.match.params;
+    axiosConfig
+      .get(`/user/viewoneuser/${id}`)
+      .then((response) => {
+        console.log("viewone_user", response.data.data);
+        this.setState({
+          fullname: response.data.data.fullname,
+          mobile: response.data.data.mobile,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  changeHandler1 = (e) => {
+    this.setState({ status: e.target.value });
+  };
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  submitHandler = (e) => {
+    e.preventDefault();
+    // let { id } = this.props.match.params
+    // console.log(id)
+    let userId = JSON.parse(localStorage.getItem("user_id"));
+    let astro_id = JSON.parse(localStorage.getItem("astroId"));
+    let obj = {
+      userid: userId,
+      astroid: astro_id,
+      fullname: this.state.fullname,
+      gender: this.state.gender,
+      tym_of_birth: this.state.tym_of_birth,
+      birth_city: this.state.birth_city,
+      birth_state: this.state.birth_state,
+      birth_country: this.state.birth_country,
+      marital_status: this.state.marital_status,
+      occupation: this.state.occupation,
+      topic_of_cnsrn: this.state.topic_of_cnsrn,
+      mobile: parseInt(this.state.mobile),
+
+      // alt_mobile: parseInt(this.state.alt_mobile),
+      // mobile: parseInt(this.state.mobile),
+      // email: this.state.email,
+    };
+
+    axiosConfig
+      .post(`/user/add_chat_intake`, obj)
+      .then((response) => {
+        console.log("@@@@@", response.data.data);
+        swal("Success!", "Submitted SuccessFull!", "success");
+        window.location.reload("/addressForm");
+      })
+
+      .catch((error) => {
+        swal("Error!", "You clicked the button!", "error");
+        console.log(error);
+      });
+  };
   render() {
+    // const { viewoneUserData } = this.state;
+
     return (
       <LayoutOne headerTop="visible">
         <section className="pt-0 pb-0">
@@ -25,9 +104,7 @@ class UserRequestForm extends React.Component {
               <Row>
                 <Col md="12">
                   <div className="leftcont text-left">
-                    <h1>
-                        Chat InTake Form
-                    </h1>
+                    <h1>Chat InTake Form</h1>
                   </div>
                 </Col>
               </Row>
@@ -49,31 +126,35 @@ class UserRequestForm extends React.Component {
                           <label>First Name*</label>
                           <input
                             type="text"
-                            name="name"
+                            name="fullname"
                             required
                             placeholder="Enter Your Fullname"
-                          
+                            value={this.state.fullname}
+                            onChange={this.changeHandler}
                           />
                         </div>
                       </Col>
-                      <Col md="4">
+                      {/* <Col md="4">
                         <div class="form-group mtb-10">
                           <label>Last Name:</label>
                           <input
                             type="text"
                             name="name"
-                            required
+                            // required
                             placeholder="Enter Your Fullname"
-                          
                           />
                         </div>
-                      </Col>
+                      </Col> */}
                       <Col md="4">
                         <div class="form-group mtb-10">
                           <label>Gender*</label>
-                          <select className="form-control">
-                              <option>Male</option>
-                              <option>Female</option>
+                          <select
+                            className="form-control"
+                            value={this.state.gender}
+                            onChange={this.changeHandler}
+                          >
+                            <option>Male</option>
+                            <option>Female</option>
                           </select>
                         </div>
                       </Col>
@@ -83,9 +164,10 @@ class UserRequestForm extends React.Component {
                           <input
                             type="number"
                             name="mobile"
-                            required
+                            value={this.state.mobile}
+                            onChange={this.changeHandler}
+                            // required
                             placeholder="Enter Your Number"
-                           
                           />
                         </div>
                       </Col>
@@ -94,10 +176,11 @@ class UserRequestForm extends React.Component {
                           <label>Date of Birth*</label>
                           <input
                             type="date"
-                            name="mobile"
-                            required
+                            name="dob"
+                            value={this.state.dob}
+                            onChange={this.changeHandler}
+                            // required
                             placeholder="Enter Your Number"
-                           
                           />
                         </div>
                       </Col>
@@ -106,10 +189,11 @@ class UserRequestForm extends React.Component {
                           <label>Time of Birth*</label>
                           <input
                             type="time"
-                            name="mobile"
-                            required
+                            name="tym_of_birth"
+                            value={this.state.tym_of_birth}
+                            onChange={this.changeHandler}
+                            // required
                             placeholder="Enter Your Number"
-                           
                           />
                         </div>
                       </Col>
@@ -118,23 +202,25 @@ class UserRequestForm extends React.Component {
                           <label>Place of Birth City*</label>
                           <input
                             type="text"
-                            name="email"
-                            required
+                            name="birth_city"
+                            value={this.state.birth_city}
+                            onChange={this.changeHandler}
+                            // required
                             placeholder="Enter Your email"
-                            
                           />
                         </div>
                       </Col>
-                     
+
                       <Col md="4">
                         <div class="form-group mtb-10">
                           <label>Place of Birth State*</label>
                           <input
                             type="text"
-                            name="alt_mobile"
-                            required
+                            name="birth_state"
+                            value={this.state.birth_state}
+                            onChange={this.changeHandler}
+                            // required
                             placeholder="Enter Your Number"
-                            
                           />
                         </div>
                       </Col>
@@ -143,44 +229,57 @@ class UserRequestForm extends React.Component {
                           <label>Place of Birth Country*</label>
                           <input
                             type="text"
-                            name="flat_no"
-                            required
+                            name="birth_country"
+                            value={this.state.birth_country}
+                            onChange={this.changeHandler}
+                            // required
                             placeholder="Enter Your Number"
-                            
                           />
                         </div>
                       </Col>
                       <Col md="4">
                         <div class="form-group mtb-10">
                           <label>Marital Status*</label>
-                          <select className="form-control">
-                              <option>lorem</option>
-                              <option>lorem</option>
+                          <select
+                            className="form-control"
+                            value={this.state.marital_status}
+                            onChange={this.changeHandler}
+                          >
+                            <option>lorem</option>
+                            <option>lorem</option>
                           </select>
                         </div>
                       </Col>
                       <Col md="4">
                         <div class="form-group mtb-10">
                           <label>Occupation*</label>
-                          <select className="form-control">
-                              <option>lorem</option>
-                              <option>lorem</option>
+                          <select
+                            className="form-control"
+                            value={this.state.occupation}
+                            onChange={this.changeHandler}
+                          >
+                            <option>lorem</option>
+                            <option>lorem</option>
                           </select>
                         </div>
                       </Col>
                       <Col md="4">
                         <div class="form-group mtb-10">
                           <label>Topic of concern*</label>
-                          <select className="form-control">
-                              <option>lorem</option>
-                              <option>lorem</option>
+                          <select
+                            className="form-control"
+                            value={this.state.topic_of_cnsrn}
+                            onChange={this.changeHandler}
+                          >
+                            <option>lorem</option>
+                            <option>lorem</option>
                           </select>
                         </div>
                       </Col>
-                     
+
                       <Col md="12" className="mt-3">
                         <Button className="btn btn-warning">
-                           Start chat  with Mukesh07
+                          Start chat with Mukesh07
                         </Button>
                       </Col>
                     </Row>
